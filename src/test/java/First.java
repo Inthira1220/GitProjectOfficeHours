7b15eeb1168b93e5fbc792d9d26c51ff4eb38600
Inthira K <kefind@gmail.com>
2021-05-25 00:15:58 -0400

    2nd commit, added Second class finish tst cast tc0001

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,5 +2,7 @@ public class First {
     /*
     this is the first class
     will publish after
+
+    added in line from First class
      */
 }
---
+++ src/test/java/Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    /*
+    2nd file, added some things
+
+    remove inline from First class
+
+     */
+}
